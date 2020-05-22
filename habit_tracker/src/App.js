22293a6c24bf9dbd1eb596beb01392fb7115e88e
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-05-21 17:53:16 -0700

    Remove everything

--- habit_tracker/src/App.js
+++ habit_tracker/src/App.js
@@ -1,29 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div class="header"></div>
-      <div class="calender_left">
-        <div class="calender1">
-          <div class="one">1</div>
-          <div class="two">2</div>
-          <div class="three">3</div>
-          <div class="four">4</div>
-        </div>
-      </div>
-      <div class="calender_right">
-        <div class="calender2">
-          <div class="one">5</div>
-          <div class="two">6</div>
-          <div class="three">7</div>
-          <div class="four">8</div>
-        </div>
-      </div>
-      <div class="footer"></div>
-    </div>
-  );
+  let [toggle, setToggle] = useState(true);
+
+  return <div className="App">Empty App.</div>;
 }
 
 export default App;
