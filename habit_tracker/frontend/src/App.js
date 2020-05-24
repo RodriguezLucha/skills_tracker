26d36750f0b2ab3af1db67c08da611477fec7749
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-05-23 17:43:14 -0700

    added months

--- habit_tracker/frontend/src/App.js
+++ habit_tracker/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useStorageState } from 'react-storage-hooks';
 
 import EasyEdit, {Types} from 'react-easy-edit';
-import {useSpring, animated} from 'react-spring'
 
 import "./App.css";
 
@@ -12,15 +12,31 @@ const snakecase = (str) =>
     .map((x) => x.toLowerCase())
     .join("_");
 
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
 function App() {
   const [days, setDays] = useState({});
   const [toggle, setToggle] = useState(true);
   const [zoomed, setZoomed] = useState(true);
 
   const [calenderName, setCalenderName] = useState("");
+  const [currrentMonth, setMonth, writeError] = useStorageState( localStorage, 'state_month', 'May' );
 
   async function fetchData() {
-    const res = await fetch("calender/month/May");
+    const res = await fetch(`calender/month/${currrentMonth}`);
     res.json().then((res) => setDays(res.data));
   }
 
@@ -71,19 +87,29 @@ function App() {
     setToggle(!toggle);
   }
 
-  const props = useSpring({
+  const props = {
     height: zoomed ? 150 : 30,
-    width: zoomed ? 150 : 70,
+    width: zoomed ? 150 : 40,
     border: '1px solid black',
     margin: 2,
     padding: 10
-  });
+  };
 
   return (
     <div className="App">
       <button onClick={() => setZoomed(!zoomed)}>
         <i className="fas fa-search-plus"></i>
       </button>
+      <div className="month_list">
+      {
+        months.map(mon => <div key={mon} className="hover_over" 
+        onClick={()=> {
+          setMonth(mon);
+          setToggle(!toggle);
+        }}>{mon}</div>)
+      }
+      </div>
+
       {Object.keys(days).map((calender_id) => {
         let name = days[calender_id].name;
         let month = days[calender_id].month;
@@ -93,7 +119,6 @@ function App() {
             <div className="cal_header">
               <h2>{month}</h2>
               <h1>{name}</h1>
-
               <button onClick={() => handleCalenderDelete(calender_id)}>
                 <i className="fa fa-trash"></i>
               </button>
@@ -103,11 +128,11 @@ function App() {
                 let day_obj = days[calender_id].days[sday];
                 let status = snakecase(day_obj.status);
                 return (
-                  <animated.div key={day_obj.day_id} style={props} className={status}>
+                  <div key={day_obj.day_id} style={props} className={status}>
                     <div className="one_day" key={day_obj.day_id}>
                       <div className="one_day_header">
                         <div>{day_obj.day}</div>
-                        <div>{day_obj.day_of_week}</div>
+                        {zoomed && <div>{day_obj.day_of_week}</div>}
                       </div>
 
                       { zoomed &&
@@ -118,8 +143,8 @@ function App() {
                           placeholder="___________________"
                           onSave={(text) => handleFocusOut(day_obj.day_id, text)}
                           saveButtonLabel="Save"
-                          hideSaveButton="1"
-                          hideCancelButton="1"
+                          hideSaveButton={true}
+                          hideCancelButton={true}
                           onHoverCssClass="hover_class"
                           cancelButtonLabel="Cancel"
                         />
@@ -131,7 +156,7 @@ function App() {
                       </>
                       }
                     </div>
-                  </animated.div>
+                  </div>
                 );
               })}
             </div>
