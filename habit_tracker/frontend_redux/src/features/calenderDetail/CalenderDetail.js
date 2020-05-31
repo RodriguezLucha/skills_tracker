3df0d9b10ed2d9d7eac6e3af742207b711705bed
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-05-31 13:43:34 -0700

    add css modules and styles

--- habit_tracker/frontend_redux/src/features/calenderDetail/CalenderDetail.js
+++ habit_tracker/frontend_redux/src/features/calenderDetail/CalenderDetail.js
@@ -1,17 +1,36 @@
 import React from "react";
 import {Link, useParams} from "react-router-dom"
+import {useSelector} from "react-redux"
 import {selectCalenderById} from "../calender/calenderSlice";
+import {selectDayEntities} from "../day/daySlice";
 
 export function CalenderDetail() {
     let {id} = useParams();
 
-    // let calender = useSelector(state => selectCalenderById(state, id));
-    console.log(id);
+    let calender = useSelector(state => selectCalenderById(state, id));
+    let day_ids = calender.days;
+    let dayEntities = useSelector(selectDayEntities);
+    let days = day_ids.map(id => dayEntities[id]);
+    
     
     return (
         <div>
             <Link to="/">Back</Link>
-            Details...
+            <div className="days_container">
+                {
+                    days.map(day => {
+                        return (
+                        <div key={day.id} className="square_big">
+                            <div className="one_day">
+                                <div className="day_number">{day.day}</div>
+                                <div className="day_of_week">{day.day_of_week}</div>
+                                <div className="day_note">{day.note}</div>
+                            </div>
+                        </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- habit_tracker/frontend_redux/src/features/monthlyCalenderInfo/Days.js
+++ habit_tracker/frontend_redux/src/features/monthlyCalenderInfo/Days.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import {selectDayEntities} from "../day/daySlice";
+import styles from './Days.module.css';
+import classnames from "classnames";
+
+const statusToStyle = {
+    "Not Set" : "none",
+    "Complete" : "green",
+    "Incomplete" : "red"
+}
 
 export function Days({days_ids}) {
     let dayEntities = useSelector(selectDayEntities);
@@ -10,13 +18,13 @@ export function Days({days_ids}) {
     const days = days_ids.map(id => dayEntities[id]);
 
     return (
-        <div className="days_container">
+        <div className={styles.container}>
             {days.map(day => {
+                let squareColorStyle = styles[statusToStyle[day.status]];
                 return (
-                <div key={day.id} className="square" >
-                    <div className="one_day">
+                <div key={day.id} className={classnames(styles.square, squareColorStyle)} >
+                    <div className={styles.single_day}>
                         <div>{day.day}</div>
-                        <div>{day.day_of_week}</div>
                     </div>
                 </div>
                 )
