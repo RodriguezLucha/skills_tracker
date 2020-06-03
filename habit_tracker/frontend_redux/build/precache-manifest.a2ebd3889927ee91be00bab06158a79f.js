22192bffb6bc256b5f1c6e54bc2aab742cd399fc
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-06-02 21:35:31 -0700

    .

---
+++ habit_tracker/frontend_redux/build/precache-manifest.a2ebd3889927ee91be00bab06158a79f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e5c72b0baa71a8cb4ca2ca784afd4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfdca9d61d7be81d4649",
+    "url": "/static/css/main.3f591d44.chunk.css"
+  },
+  {
+    "revision": "7eff73040a6ab95274f0",
+    "url": "/static/js/2.e4f26653.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e4f26653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfdca9d61d7be81d4649",
+    "url": "/static/js/main.9ffbe8dd.chunk.js"
+  },
+  {
+    "revision": "9e3e953ca55579cbeb24",
+    "url": "/static/js/runtime-main.59253d89.js"
+  }
+]);
